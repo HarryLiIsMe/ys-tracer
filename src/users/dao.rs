37736b3750f77f5baa91707c0e6997d1f774b837
18328594608 <1007697416@qq.com>
2022-07-21 15:34:02 +0800

    新增邮件逻辑

--- src/users/dao.rs
+++ src/users/dao.rs
@@ -70,6 +70,16 @@ pub trait IUser: std::ops::Deref<Target = AppStateRaw> {
 
         sqlx::query(&sql).bind(address).execute(&self.sql).await.map(|d| d.rows_affected())
     }
+
+    async fn mail_update(&self, address: &str, post_email: &str) ->sqlx::Result<u64> {
+
+        let sql = format!(
+            "update user_address2 set post_email='{}' where address='{}';",
+            post_email, address
+        );
+
+        sqlx::query(&sql).bind(address).execute(&self.sql).await.map(|d| d.rows_affected())
+    }
 }
 
 #[cfg(any(feature = "postgres"))]
